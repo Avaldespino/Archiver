f99c70a3134fbdbf1a64575053a1542b3aaffbf2
Adrian <avaldespino@miners.utep.edu>
2022-03-10 21:57:18 -0700

    Got some encode to bin to work

--- archive.py
+++ archive.py
@@ -5,20 +5,32 @@ if len(sys.argv) == 1:
     os.write(2, ("No files are given\n").encode())
     sys.exit(1)
 name = str(sys.argv[1])
-fileOut = os.open(name, os.O_CREAT | os.O_WRONLY)
+out = name.split('.')
+out = out[0]+".arch"
+fileOut = os.open(out, os.O_CREAT | os.O_WRONLY)
 
-for i in range(2,len(sys.argv)):
+for i in range(1,len(sys.argv)):
    with open(sys.argv[i], 'r') as f:
       contents = f.read()
-   byteContents = contents.encode()
-   size = str(len(byteContents)).encode()
-   output = size+byteContents
-   output = output.bytearray()
-   os.write(fileOut, output)
+   byteList = []   
+   for char in contents.encode():
+       byteList.append(bin(char)
+   
+   os.write(fileOut, byteList)
    
                
-test = "Hello world"
+#test = "Hello world"
 
-byte = test.encode()
+#test_output = test.encode()
+#byteList = []
+#for char in test_output:
+ #   byteList.append(bin(char))
 
-print(byte)
+#print(byteList)
+
+
+#for byte in byteList:
+    #print('%x' % int(byte,2)).decode('hex').decode('utf-8')
+
+    #print ('%x' % int(byte, 2)).decode('hex').decode('utf-8')
+ #   print(chr(int(byte,2)))
